e634651b4d9c310db662135d793693ca1d61a690
Filip <filip.spanberg@gmail.com>
2019-03-14 15:38:22 +0100

    En till klass!

--- ShowTracker/src/test/Test.java
+++ ShowTracker/src/test/Test.java
@@ -1,9 +1,8 @@
 package test;
 
 public class Test {
-	public static void main(String[] args) {
-	int x = 0;
-	System.out.println(x);
-	}
-
+    public static void main(String[] args) {
+        int x = 0;
+        System.out.println(x);
+    }
 }
