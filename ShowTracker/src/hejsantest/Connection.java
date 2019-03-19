34eabc9a8d838b5ea5bcfff96ae1cfe531fd441a
Basir98 <47216998+Basir98@users.noreply.github.com>
2019-03-19 13:05:06 +0100

    en ny conneciton klass med några variabel

--- ShowTracker/src/hejsantest/Connection.java
+++ ShowTracker/src/hejsantest/Connection.java
@@ -1,5 +1,6 @@
 package hejsantest;
 
 public class Connection {
-
+	int x =1;
+	String b = "hej";
 }
--- ShowTracker/src/hejsantest/fffffff.java
+++ ShowTracker/src/hejsantest/fffffff.java
@@ -5,5 +5,6 @@ import javax.swing.JOptionPane;
 public class fffffff {
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "Fuckoff lad");
+		
 	}
 }
