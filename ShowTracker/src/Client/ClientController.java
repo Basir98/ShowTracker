edd4de42bb0b038161567ecb0e1d68e96f3c1b56
adamj <adamj@adams-mbp.clients.internal.mah.se>
2019-03-21 12:00:26 +0100

    login test

--- ShowTracker/src/Client/ClientController.java
+++ ShowTracker/src/Client/ClientController.java
@@ -1,12 +1,29 @@
 package Client;
 
+import java.io.*;
+import java.util.*;
+
+import javax.swing.JOptionPane;
+
 public class ClientController {
 
-	public void signIn() {
+	private String username, userPassword, email;
+	
+	
+	public void signIn(String usernameInp, String userPasswordInp) throws FileNotFoundException {
 		
+		Scanner scan = new Scanner (new File("files/befolkning.txt"));
+	    String user = scan.nextLine();
+	    String pass = scan.nextLine(); 
+
+	    if (usernameInp.equals(user) && userPasswordInp.equals(pass)) {
+	        JOptionPane.showMessageDialog(null, "Signed in!");
+	    } else {
+	    	 JOptionPane.showMessageDialog(null, "your error message");
+	    }
 	}
 
-	public void signUp() {
+	public void signUp(String username, String userPassword, String email) {
 		
 	}
 
