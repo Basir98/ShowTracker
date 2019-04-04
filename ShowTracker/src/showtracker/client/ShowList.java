66a8cfd8e2f8eb3b4f0771e996df3e502fb76aa9
Basir98 <47216998+Basir98@users.noreply.github.com>
2019-04-04 11:26:20 +0200

    ShowList klassen fungerar som det ska och listar listor på shower användaren har i sin lista

--- ShowTracker/src/showtracker/client/ShowList.java
+++ ShowTracker/src/showtracker/client/ShowList.java
@@ -14,11 +14,10 @@ public class ShowList extends JFrame {
 	ClientController clientController = new ClientController();
 
 	private JLabel infoLabel1;
-	private JLabel label;
-
 	private ArrayList<ClientController> show = new ArrayList<>();
 	private JPanel jpShowList = new JPanel(new GridLayout(10, 2));
 	private JPanel searchBarJP = new JPanel();
+	ArrayList<JButton> btnList = new ArrayList<>();
 
 	public ShowList() throws FileNotFoundException {
 		clientController.fyllShows();
@@ -34,59 +33,6 @@ public class ShowList extends JFrame {
 //		setSize(new Dimension(350, 500));
 
 	}
-
-	/*
-	 * public JPanel filedPanel() { // JPanel panel = new JPanel(); //
-	 * panel.setLayout(new GridLayout(2, 2, 1, 1));
-	 * 
-	 * btn1.setVisible(false);
-	 * 
-	 * infoLabel1.setLayout(new BorderLayout()); infoLabel1.setBorder(new
-	 * LineBorder(Color.GRAY, 1)); infoLabel1.setPreferredSize(new Dimension(200,
-	 * 30)); infoLabel1.add(btn1, BorderLayout.EAST);
-	 * 
-	 * btn1.addMouseListener(new MouseAdapter() { public void mouse(MouseEvent e) {
-	 * Point mousePosition = MouseInfo.getPointerInfo().getLocation(); if
-	 * (infoLabel1.contains(mousePosition)) { infoLabel1.dispatchEvent(new
-	 * MouseEvent(infoLabel1, MouseEvent.MOUSE_ENTERED, System.currentTimeMillis(),
-	 * 0, mousePosition.x, mousePosition.y, 0, false));
-	 * 
-	 * } else { infoLabel1.dispatchEvent(new MouseEvent(infoLabel1,
-	 * MouseEvent.MOUSE_EXITED, System.currentTimeMillis(), 0, mousePosition.x,
-	 * mousePosition.y, 0, false)); } }
-	 * 
-	 * });
-	 * 
-	 * btn1.addActionListener(new ActionListener() { public void
-	 * actionPerformed(ActionEvent e) { JOptionPane.showMessageDialog(null,
-	 * "The button was pushed!", "    Info", JOptionPane.PLAIN_MESSAGE); Point
-	 * mousePosition = MouseInfo.getPointerInfo().getLocation();
-	 * infoLabel1.dispatchEvent(new MouseEvent(infoLabel1, MouseEvent.MOUSE_EXITED,
-	 * System.currentTimeMillis(), 0, mousePosition.x, mousePosition.y, 0, false));
-	 * } });
-	 * 
-	 * infoLabel1.addMouseListener(new MouseAdapter() { public void
-	 * mouseEntered(MouseEvent e) { JLabel label = (JLabel) e.getSource(); //
-	 * label.setText(""); btn1.setVisible(true);
-	 * 
-	 * }
-	 * 
-	 * public void mouseExited(MouseEvent e) { Point point = e.getPoint();
-	 * point.setLocation(point.x - btn1.getX(), point.y - btn1.getY());
-	 * 
-	 * if (!btn1.contains(point)) { JLabel label = (JLabel) e.getSource();
-	 * btn1.setVisible(false); } }
-	 * 
-	 * });
-	 * 
-	 * JPanel panel = new JPanel(); panel.setLayout(new FlowLayout());
-	 * 
-	 * panel.add(infoLabel1);
-	 * 
-	 * setContentPane(panel); return panel;
-	 * 
-	 * }
-	 */
 
 	private JPanel drawSearchBarPanel() {
 		searchBarJP.setBackground(Color.GREEN);
@@ -117,6 +63,7 @@ public class ShowList extends JFrame {
 		int i = 1;
 		for (Show s : myShows) {
 			JButton button = new JButton("Info" + i);
+			btnList.add(button);
 			button.setVisible(false);
 
 			jpShowList.add(infoLabel1 = new JLabel(s.getName()));
@@ -124,55 +71,9 @@ public class ShowList extends JFrame {
 
 			infoLabel1.setBorder(new LineBorder(Color.GRAY, 1));
 
-			button.addMouseListener(new MouseAdapter() {
+			button.addMouseListener(new ButtonAdapter());
+			infoLabel1.addMouseListener(new LabelAdapter(button));
 
-				public void mouseExited(MouseEvent e) {
-					Point mousePosition = MouseInfo.getPointerInfo().getLocation();
-					if (infoLabel1.contains(mousePosition)) {
-						infoLabel1.dispatchEvent(new MouseEvent(infoLabel1, MouseEvent.MOUSE_ENTERED,
-								System.currentTimeMillis(), 0, mousePosition.x, mousePosition.y, 0, false));
-					} else {
-						infoLabel1.dispatchEvent(new MouseEvent(infoLabel1, MouseEvent.MOUSE_EXITED,
-								System.currentTimeMillis(), 0, mousePosition.x, mousePosition.y, 0, false));
-					}
-				}
-
-			});
-
-			button.addActionListener(new ActionListener() {
-
-				public void actionPerformed(ActionEvent e) {
-					JOptionPane.showMessageDialog(null, "The button was pressed!");
-					Point mousePosition = MouseInfo.getPointerInfo().getLocation();
-					infoLabel1.dispatchEvent(new MouseEvent(infoLabel1, MouseEvent.MOUSE_EXITED,
-							System.currentTimeMillis(), 0, mousePosition.x, mousePosition.y, 0, false));
-				}
-			});
-
-			infoLabel1.addMouseListener(new MouseAdapter() {
-				@Override
-				public void mouseEntered(MouseEvent e) {
-//					JLabel label = (JLabel) e.getSource();
-//					label =(JLabel)e.getSource();
-
-					infoLabel1 = (JLabel) e.getSource();
-//					infoLabel1.setText("Here is the Button!");
-					button.setVisible(true);
-				}
-
-				@Override
-				public void mouseExited(MouseEvent e) {
-					Point point = e.getPoint();
-					point.setLocation(point.x - button.getX(), point.y - button.getY()); // make the point relative to
-																							// the // button's location
-					if (!button.contains(point)) {
-//						JLabel label = (JLabel) e.getSource();
-						label = (JLabel) e.getSource();
-//						label.setText("The button is gone!");
-						button.setVisible(false);
-					}
-				}
-			});
 			jpShowList.revalidate();
 			i++;
 		}
@@ -189,7 +90,25 @@ public class ShowList extends JFrame {
 			}
 
 		});
-
 	}
 
+	private class LabelAdapter extends MouseAdapter {
+		private JButton button;
+
+		public LabelAdapter(JButton button) {
+			this.button = button;
+		}
+
+		public void mouseEntered(MouseEvent e) {
+			for (JButton b : btnList)
+				b.setVisible(false);
+			button.setVisible(true);
+		}
+	}
+
+	private class ButtonAdapter extends MouseAdapter {
+		public void mouseExited(MouseEvent e) {
+			((JButton) e.getSource()).setVisible(false);
+		}
+	}
 }
